02f057f9f88547d7ecef350b435a5e7752121cff
mazmaz2k <mazmaz2k@gmail.com>
2017-06-13 20:34:27 +0300

    fix app code apter code review v5.2

--- JLM.FM2/app/src/main/java/com/radio/fm/jlm/jlmfm2/sendingEmail.java
+++ JLM.FM2/app/src/main/java/com/radio/fm/jlm/jlmfm2/sendingEmail.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class sendingEmail extends AppCompatActivity {
+public class SendingEmail extends AppCompatActivity {
 
 
     @Override
