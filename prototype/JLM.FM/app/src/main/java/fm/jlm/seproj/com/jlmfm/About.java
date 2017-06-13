b56375ee77e0fbf4d2620ac9d8ee7a7739d74c9b
mazmaz2k <mazmaz2k@gmail.com>
2017-06-13 20:37:50 +0300

    Create About.java

--- prototype/JLM.FM/app/src/main/java/fm/jlm/seproj/com/jlmfm/About.java
+++ prototype/JLM.FM/app/src/main/java/fm/jlm/seproj/com/jlmfm/About.java
@@ -3,7 +3,7 @@ package fm.jlm.seproj.com.jlmfm;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Activity;
+//import android.app.Activity;
 import android.os.Bundle;
 //import android.view.View;
 //import android.view.ViewGroup.LayoutParams;
